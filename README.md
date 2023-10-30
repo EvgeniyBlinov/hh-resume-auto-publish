130b64fb0f2a69c8785cdd8be7efa3b9171cf8bb
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2023-10-30 14:05:56 +0300

    blinov
    Work stage 2023-10-30 14:05:56
     M README.md
     M get_user_tokens.sh
     M updater.py

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Script to update your resumes (CV) on hh.ru automatically.
 0. Check installed packages(jq, awk, sed, docker, docker-compose)
 1. Get tokens:
   * Go to `https://dev.hh.ru/admin` and register app for get <client_id> and <client_secret>
-  * Go to `https://hh.ru/oauth/authorize?response_type=code&client_id=<client_id>` for get `https://hh.ru/?code=<code>`
+  * Go to `https://hh.ru/oauth/authorize?response_type=code&client_id=${CLIENT_ID}` for get `https://hh.ru/?code=<code>`
   * Put them all to `.env`
 2. `bash get_user_tokens.sh`
 3. `docker-compose up -d`
--- updater.py
+++ updater.py
@@ -5,62 +5,118 @@ import requests
 import logging
 import json
 
-log = logging.getLogger('hh-resume-auto-publish')
-log.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-ch = logging.StreamHandler()
-ch.setFormatter(formatter)
-log.addHandler(ch)
+class HH(object):
 
+    """Docstring for HH. """
 
-def get_resume_list():
-    url = 'https://api.hh.ru/resumes/mine'
-    r = s.get(url)
+    def __init__(self, s: requests.sessions.Session, client_id: str, client_secret: str, code: str, api_token: str, ref_token: str, resume_id: str):
+        """TODO: to be defined. """
+        self.s = s
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.code = code
+        self.api_token = api_token
+        self.ref_token = ref_token
 
-    if r.status_code == 200:
-        data = r.json()
-        resume_ids = [resume['id'] for resume in data['items']]
-        log.info('Loaded resume list ({0} items)'. format(len(resume_ids)))
-        return resume_ids
-    else:
-        msg = "Can't get resume list from hh.ru!"
-        log.error(msg)
-        exit()
+        s.headers.update({'Authorization': f'Bearer {self.api_token}'})
+
+        if resume_id:
+            self.resume_id = resume_id
+        else:
+            self.resume_id = self.get_resume_list()
 
 
-def update_resume(resume_id):
-    url = f'https://api.hh.ru/resumes/{resume_id}/publish'
-    r = s.post(url)
+    def get_resume_list(self):
+        url = 'https://api.hh.ru/resumes/mine'
+        r = self.s.get(url)
 
-    if r.status_code == 204:
-        log.info(f'{resume_id}: updated')
-    elif r.status_code == 429:
-        log.info(f'{resume_id}: too many requests')
-    elif r.status_code == 400:
-        log.info(f'{resume_id}: can\'t update because resume is incorrect')
-    elif r.status_code == 403:
-        log.error(f'{resume_id}: auth required')
-    else:
-        log.error(f'{resume_id}: unknown status')
+        if r.status_code == 200:
+            data = r.json()
+            resume_ids = [resume['id'] for resume in data['items']]
+            log.info('Loaded resume list ({0} items)'. format(len(resume_ids)))
+            return resume_ids
+        else:
+            msg = "Can't get resume list from hh.ru!"
+            log.error(msg)
+            exit()
 
 
-def update_token():
-    global ref_token
-    global api_token
-    url='https://hh.ru/oauth/token'
-    data={'grant_type': 'refresh_token',
-          'refresh_token': ref_token}
-    r=requests.post(url = url, data = data)
 
-    if r.status_code == 200:
-        tokens=r.json()
-        api_token=tokens['access_token']
-        ref_token=tokens['refresh_token']
-        log.info('token updated')
-        s.headers.update({'Authorization': f'Bearer {api_token}'})
-    elif r.status_code == 400:
-        log.info('token not expired')
+    def update_resume(self):
+        resume_id = self.resume_id
+        url = f'https://api.hh.ru/resumes/{resume_id}/publish'
+        s.headers.update({'Authorization': f'Bearer {self.api_token}'})
+        r = s.post(url)
+
+        if r.status_code == 204:
+            log.info(f'update_resume {resume_id}: updated')
+        elif r.status_code == 429:
+            log.info(f'update_resume {resume_id}: too many requests')
+        elif r.status_code == 400:
+            log.info(f'update_resume {resume_id}: can\'t update because resume is incorrect')
+        elif r.status_code == 403:
+            data = r.json()
+            log.error(f'update_resume {resume_id}: auth required: %s' % data)
+            oauth_bad_authorization = {'value': 'bad_authorization', 'type': 'oauth'}
+            if 'errors' in data and oauth_bad_authorization in data['errors']:
+                self.update_authorization_code()
+        else:
+            log.error(f'update_resume {resume_id}: unknown status: %s' % r.json())
+
+
+    def update_token(self):
+        url='https://hh.ru/oauth/token'
+        data={'grant_type': 'refresh_token',
+            'refresh_token': self.ref_token}
+
+        r=requests.post(url = url, data = data)
+
+        if r.status_code == 200:
+            tokens=r.json()
+            self.api_token=tokens['access_token']
+            self.ref_token=tokens['refresh_token']
+            log.info('update_token: token updated')
+            s.headers.update({'Authorization': f'Bearer {self.api_token}'})
+        elif r.status_code == 400:
+            data = r.json()
+            log.info('update_token: token not expired: %s' % data)
+
+
+    def update_authorization_code(self):
+        url='https://hh.ru/oauth/token'
+        data={'grant_type': 'authorization_code',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'code': self.code}
+
+        r=requests.post(url = url, data = data)
+
+
+        if r.status_code == 200:
+            tokens=r.json()
+            self.api_token=tokens['access_token']
+            self.ref_token=tokens['refresh_token']
+            log.info('token updated')
+            s.headers.update({'Authorization': f'Bearer {self.api_token}'})
+        elif r.status_code == 400:
+            log.info('token not expired')
+            # log.error('update_authorization_code: %s' % r.json())
+
+
+    def update(self):
+        while True:
+            # check updatability token before request to API
+            self.update_token()
+
+            for id in self.resume_id:
+                self.update_resume()
+                time.sleep(5)
+
+            # sleep 60 minutes
+            log.info('going sleep...')
+            time.sleep(60 * 60)
+
 
 
 # check if hh.ru API tokens is in environment variables
@@ -71,30 +127,25 @@ else:
     sys.exit("hh.ru API token is not specified! "
              "Go to read README.md",)
 
+# if 'CLIENT_ID' in os.environ and 'CLIENT_SECRET' in os.environ and 'CODE' in os.environ:
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
+code = os.environ['CODE']
+resume_id = os.getenv('HH_RESUME_ID', '') 
+
+log = logging.getLogger('hh-resume-auto-publish')
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+log.addHandler(ch)
 
 s = requests.Session()
-s.headers.update({'Authorization': f'Bearer {api_token}'})
 
-
-# check if hh.ru resume ID is in environment variables
-if 'HH_RESUME_ID' in os.environ:
-    # then update only that resume
-    resume_id_list = [os.environ['HH_RESUME_ID']]
-else:
-    # else update all available resumes
-    resume_id_list = get_resume_list()
+hh = HH(s, client_id, client_secret, code, api_token, ref_token, resume_id)
 
 
 # main loop
 if __name__ == '__main__':
-    while True:
-        # check updatability token before request to API
-        update_token()
-
-        for id in resume_id_list:
-            update_resume(id)
-            time.sleep(5)
-
-        # sleep 60 minutes
-        log.info('going sleep...')
-        time.sleep(60 * 60)
+    hh.update()
